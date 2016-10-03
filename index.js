3bf8a5ac401121617b44f479e21e2491d34d576b
Vladimir Halme <vhalme@gmail.com>
2016-10-04 01:16:51 +0300

    Added script running

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+const spawn = require('child_process').spawn;
+
 var express = require('express');
 var app = express();
 
 app.post('/docker', function (req, res) {
-  console.log('Hook hit', req);
-  res.send('Hello World!');
+  
+  console.log('Redeploying...');
+  const redeploy = spawn('./deploy.sh');
+
+  redeploy.on('close', (code) => {
+    console.log(`Redeploy exited with code ${code}`);
+  });
+  
+  res.send('OK');
+
 });
 
 app.listen(3005, function () {
